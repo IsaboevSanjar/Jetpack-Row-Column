b95720638ee814feb136aa4fa339fe2864587a9e
SanjarIsaboev <sanjarisaboev@yandex.com>
2024-04-05 15:11:48 +0500

    Text
    Signed-off-by: SanjarIsaboev <sanjarisaboev@yandex.com>

--- app/src/main/java/sanjar/uz/myapplication/MainActivity.kt
+++ app/src/main/java/sanjar/uz/myapplication/MainActivity.kt
@@ -4,17 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -38,22 +39,17 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting() {
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.TopCenter) {
-        Box(
+    Column(modifier = Modifier.fillMaxSize()) {
+        Text(
+            text = stringResource(id = R.string.app_name),
             modifier = Modifier
-                .background(Color.Blue)
-
-        ) {
-            Box(
-                modifier = Modifier
-                    .height(50.dp)
-                    .width(50.dp)
-                    .background(Color.Green)
-            ) {
-
-            }
-            Text(text = "I love Android!!!", fontSize = 40.sp)
-        }
+                .background(MaterialTheme.colorScheme.primary)
+                .padding(8.dp),
+            color = Color.White,
+            fontSize = 20.sp,
+            fontFamily = FontFamily.Cursive,
+            fontWeight = FontWeight.Bold
+        )
     }
 }
 
