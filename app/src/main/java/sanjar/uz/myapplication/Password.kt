3fe6c51cdf699d6c7c07210d99c637c201ce1d3e
SanjarIsaboev <sanjarisaboev@yandex.com>
2024-04-07 14:35:09 +0500

    Password eye icon finish
    Signed-off-by: SanjarIsaboev <sanjarisaboev@yandex.com>

--- app/src/main/java/sanjar/uz/myapplication/Password.kt
+++ app/src/main/java/sanjar/uz/myapplication/Password.kt
@@ -1,5 +1,7 @@
 package sanjar.uz.myapplication
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
@@ -11,7 +13,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun Password() {
@@ -37,11 +44,17 @@ fun Password() {
             trailingIcon = {
                 IconButton(onClick = { passwordVisibility = !passwordVisibility }) {
                     Icon(
-                        painter = painterResource(id = R.drawable.google_icon),
-                        contentDescription = "visibility"
+                        painter = icon,
+                        contentDescription = "visibility",
+                        modifier = Modifier.padding(7.dp)
                     )
                 }
-            }
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Password
+            ),
+            visualTransformation = if (passwordVisibility) VisualTransformation.None
+            else PasswordVisualTransformation()
         )
     }
 }
