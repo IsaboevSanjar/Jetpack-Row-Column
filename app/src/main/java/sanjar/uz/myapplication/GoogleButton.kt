ad7a333bef3b5e9ca037eac58660f3c1b707d15b
SanjarIsaboev <sanjarisaboev@yandex.com>
2024-04-07 12:41:19 +0500

    Google Button 2 it's useful to sign up with google button
    Signed-off-by: SanjarIsaboev <sanjarisaboev@yandex.com>

--- app/src/main/java/sanjar/uz/myapplication/GoogleButton.kt
+++ app/src/main/java/sanjar/uz/myapplication/GoogleButton.kt
@@ -1,11 +1,16 @@
 package sanjar.uz.myapplication
 
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ShapeDefaults
@@ -19,36 +24,64 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun GoogleButton() {
+fun GoogleButton(
+    textGoogle: String = "Sign Up with Google",
+    loadingText: String = "Account is creating...",
+    icon: Painter = painterResource(id = R.drawable.google_icon),
+    shape: Shape = ShapeDefaults.Medium,
+    borderColor: Color = Color.LightGray,
+    backgroundColor: Color = MaterialTheme.colorScheme.surface,
+    onClick: () -> Unit
+) {
     var clicked by remember { mutableStateOf(false) }
     Surface(
         onClick = { clicked = !clicked },
-        shape = ShapeDefaults.Medium,
-        border = BorderStroke(width = 1.dp, color = Color.LightGray),
-        color = MaterialTheme.colorScheme.surface
+        shape = shape,
+        border = BorderStroke(width = 1.dp, color = borderColor),
+        color = backgroundColor,
     ) {
         Row(
-            modifier = Modifier.padding(
-                start = 12.dp,
-                end = 16.dp,
-                top = 10.dp,
-                bottom = 10.dp
-            ),
+            modifier = Modifier
+                .padding(
+                    start = 12.dp,
+                    end = 16.dp,
+                    top = 10.dp,
+                    bottom = 10.dp
+                )
+                .animateContentSize(
+                    animationSpec = tween(
+                        durationMillis = 300,
+                        easing = LinearOutSlowInEasing
+                    )
+                ),
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.Center
         ) {
             Icon(
-                painter = painterResource(id = R.drawable.google_icon),
+                painter = icon,
                 contentDescription = "Google Icon",
                 tint = Color.Unspecified
             )
             Spacer(modifier = Modifier.width(8.dp))
-            Text(text = "Sign Up with Google")
+            Text(text = if (clicked) loadingText else textGoogle)
+            if (clicked) {
+                Spacer(modifier = Modifier.width(8.dp))
+                CircularProgressIndicator(
+                    modifier = Modifier
+                        .height(16.dp)
+                        .width(16.dp),
+                    strokeWidth = 2.dp,
+                    color = MaterialTheme.colorScheme.primary
+                )
+                onClick()
+            }
         }
     }
 
@@ -57,5 +90,8 @@ fun GoogleButton() {
 @Composable
 @Preview
 fun GoogleButtonPreview() {
-    GoogleButton()
+    GoogleButton(
+        textGoogle = "Sign Up With Google",
+        loadingText = "Creating Account...",
+        onClick = {})
 }
--- app/src/main/java/sanjar/uz/myapplication/MainActivity.kt
+++ app/src/main/java/sanjar/uz/myapplication/MainActivity.kt
@@ -1,12 +1,17 @@
 package sanjar.uz.myapplication
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import sanjar.uz.myapplication.ui.theme.MyApplicationTheme
 
@@ -17,10 +22,20 @@ class MainActivity : ComponentActivity() {
             MyApplicationTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    color = MaterialTheme.colorScheme.background,
-                    modifier = Modifier.padding(10.dp)
+                    color = Color.White,
+                    modifier = Modifier
+                        .padding(10.dp)
                 ) {
-                    GoogleButton()
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        GoogleButton(
+                            textGoogle = "Sign Up With Google",
+                            loadingText = "Creating Account...",
+                            onClick = { Log.d("googleButton", "Clicked...") })
+                    }
                 }
             }
         }
