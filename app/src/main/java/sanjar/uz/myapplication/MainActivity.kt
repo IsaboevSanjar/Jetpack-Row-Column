defaffe35f447228000da26e1e55e83cb7225a3b
SanjarIsaboev <sanjarisaboev@yandex.com>
2024-04-15 19:51:59 +0500

    Search field is finished
    Signed-off-by: SanjarIsaboev <sanjarisaboev@yandex.com>

--- app/src/main/java/sanjar/uz/myapplication/MainActivity.kt
+++ app/src/main/java/sanjar/uz/myapplication/MainActivity.kt
@@ -3,16 +3,20 @@ package sanjar.uz.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import sanjar.uz.myapplication.searchBar.SearchViewModel
 import sanjar.uz.myapplication.searchBar.TextFields
 import sanjar.uz.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val searchViewModel: SearchViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -27,7 +31,7 @@ class MainActivity : ComponentActivity() {
                     Column(
 
                     ) {
-                        TextFields()
+                        TextFields(viewModel = searchViewModel)
                     }
                 }
             }
--- app/src/main/java/sanjar/uz/myapplication/searchBar/TextFields.kt
+++ app/src/main/java/sanjar/uz/myapplication/searchBar/TextFields.kt
@@ -2,24 +2,17 @@ package sanjar.uz.myapplication.searchBar
 
 import android.annotation.SuppressLint
 import android.util.Log
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material.icons.filled.Close
-import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -28,65 +21,67 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun TextFields() {
+fun TextFields(viewModel: SearchViewModel) {
+    val searchWidgetState by viewModel.searchWidgetState
+    val searchTextState by viewModel.searchTextState
+
     Scaffold(
         topBar = {
-            DefaultAppBar(
-                onSearchClicked = {}
+            MainAppBar(
+                searchWidgetState = searchWidgetState,
+                searchTextState = searchTextState,
+                onTextChanged = {
+                    viewModel.updateSearchTextState(it)
+                },
+                onCloseClicked = {
+                    viewModel.updateSearchWidgetState(newValue = SearchWidgetState.CLOSED)
+                },
+                onSearchClicked = {
+                    Log.d("Searching", it)
+                },
+                onSearchTriggered = {
+                    viewModel.updateSearchWidgetState(newValue = SearchWidgetState.OPENED)
+                }
             )
         }
-    ) {
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
-            var text by remember { mutableStateOf("Type here...") }
-            OutlinedTextField(
-                value = text,
-                onValueChange = { newText -> text = newText },
-                label = { Text(text = "Title") },
-                leadingIcon = {
-                    IconButton(onClick = { /*TODO*/ }) {
-                        Icon(imageVector = Icons.Default.Email, contentDescription = "Email Icon")
-                    }
-                },
-                trailingIcon = {
-                    IconButton(onClick = { /*TODO*/ }) {
-                        Icon(
-                            imageVector = Icons.Default.CheckCircle,
-                            contentDescription = "Check Icon"
-                        )
-                    }
-                },
-                keyboardOptions = KeyboardOptions(
-                    keyboardType = KeyboardType.Email,
-                    imeAction = ImeAction.Send
-                ),
-                keyboardActions = KeyboardActions(
-                    onSend = {
-                        Log.d("TAGGING", "Send clicked")
-                    }
-                )
-            )
+    ) {}
+
+}
+
+@Composable
+fun MainAppBar(
+    searchWidgetState: SearchWidgetState,
+    searchTextState: String,
+    onTextChanged: (String) -> Unit,
+    onCloseClicked: () -> Unit,
+    onSearchClicked: (String) -> Unit,
+    onSearchTriggered: () -> Unit
+) {
+    when (searchWidgetState) {
+        SearchWidgetState.CLOSED -> {
+            DefaultAppBar(onSearchClicked = onSearchTriggered)
+        }
+
+        SearchWidgetState.OPENED -> {
+            SearchAppBar(
+                text = searchTextState,
+                onTextChange = onTextChanged,
+                onCloseClicked = { onCloseClicked() }) {
+
+            }
         }
     }
-
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -127,7 +122,6 @@ fun SearchAppBar(
             .fillMaxWidth()
             .height(56.dp),
         shadowElevation = 12.dp,
-        color = MaterialTheme.colorScheme.primary
     ) {
         TextField(
             value = text,
@@ -135,8 +129,7 @@ fun SearchAppBar(
                 onTextChange(it)
             },
             modifier = Modifier
-                .fillMaxWidth()
-                .background(Color.Transparent),
+                .fillMaxWidth(),
             placeholder = {
                 Text(
                     modifier = Modifier.alpha(alpha = 0.5F),
@@ -145,7 +138,7 @@ fun SearchAppBar(
                 )
             },
             textStyle = TextStyle(
-                fontSize = MaterialTheme.typography.headlineMedium.fontSize
+                fontSize = 16.sp
             ),
             singleLine = true,
             leadingIcon = {
